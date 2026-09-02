60f2610dae1398931aada481b1416c50f64ae22a
vakye <filsamare@gmail.com>
2026-09-02 16:35:47 +0700

    Added multithreading + Added SpawnThread() and JoinThread() along with their Linux   implementation in platform.c

--- code/platform.c
+++ code/platform.c
@@ -5,6 +5,19 @@
 
 typedef usize wall_clock;
 
+typedef u32 thread_id;
+#define NilThreadID (0)
+
+typedef s32 thread_callback(void* UserData);
+
+typedef struct
+{
+    thread_callback*    Callback;           // NOTE(vak): Function that will be called when the thread spawns.
+    void*               UserData;           // NOTE(vak): User data that will be passed to the callback.
+    void*               StackBase;          // NOTE(vak): Base address of stack space. Must be aligned on a 64 byte boundary.
+    usize               StackSize;          // NOTE(vak): Size of stack space. Must be equal to or larger than 1KB. Must be aligned on a 64 byte boundary.
+} spawn_thread_info;
+
 local wall_clock    GetClockNow         (void);                                         // NOTE(vak): Get current value of the system wall clock.
 local wall_clock    ClockElapsedSince   (wall_clock Reference);                         // NOTE(vak): Get the amount of time elapsed since the `Reference` point.
 local wall_clock    ClockDifference     (wall_clock From, wall_clock To);               // NOTE(vak): Get the amount of time elapsed between two different points. `From` is assumed to be less than `To`.
@@ -17,6 +30,8 @@ local void*         ReserveMemory       (usize Size);                           
 local b32           CommitMemory        (void* Memory, usize Size);                     // NOTE(vak): Makes a virtual address range usable by mapping it to physical memory. Returns true on success, and false on failure.
 local void*         ReserveAndCommit    (usize Size);                                   // NOTE(vak): Reserves and commits a region of memory whose size is equal to or larger than the specified `Size`.
 local void          ReleaseMemory       (void* Memory, usize ReservedSize);             // NOTE(vak): Releases a virtual address space and all of its associated physical memory back to the system.
+local thread_id     SpawnThread         (spawn_thread_info* Info);                      // NOTE(vak): Spawns a new thread with the specified options.
+local s32           JoinThread          (thread_id ThreadID);                           // NOTE(vak): Waits for thread to exit. Returns exit code of thread.
 local void          ThreadExit          (u8 ExitCode);                                  // NOTE(vak): Exits calling thread with `ExitCode`
 local void          ProcessExit         (u8 ExitCode);                                  // NOTE(vak): Exits entire process with `ExitCode`
 
@@ -50,6 +65,7 @@ typedef enum
     LinuxSyscallNR_MUnMap           = (11),
     LinuxSyscallNR_Nanosleep        = (35),
     LinuxSyscallNR_Exit             = (60),
+    LinuxSyscallNR_Futex            = (202),
     LinuxSyscallNR_Clock_GetTime    = (228),
     LinuxSyscallNR_Clock_GetRes     = (229),
     LinuxSyscallNR_ExitGroup        = (231),
@@ -63,6 +79,29 @@ typedef struct
     u64 Seconds;
     u64 Nanoseconds;
 } linux_timespec;
+
+typedef struct
+{
+    void*               ThreadEntryAddress; // NOTE(vak): Always positioned first so ret instruction can pop this off.
+    thread_callback*    Callback;
+    void*               UserData;
+    thread_id           ThreadID;
+    u32                 Padding;
+} linux_stack_header;
+
+CTAssert(IsAligned(sizeof(linux_stack_header), 16));
+
+typedef struct
+{
+    s32                 JoinFutex;
+    s32                 Returned;
+} linux_thread;
+
+#define LinuxMaxThreadSpawnCount (4096)
+#define LinuxIsValidThreadID(ThreadID) (((ThreadID) > 0) && ((ThreadID) < LinuxMaxThreadSpawnCount))
+
+local linux_thread  LinuxAllThreads[LinuxMaxThreadSpawnCount]   = {0};
+local u32           LinuxThreadCount                            = 1;
 
 #define CLOCK_MONOTONIC (0)
 
@@ -79,6 +118,9 @@ typedef struct
 
 #define MAP_PRIVATE     (0x02)
 #define MAP_ANONYMOUS   (0x20)
+
+#define FUTEX_WAIT      (0)
+#define FUTEX_WAKE      (1)
 
 local usize LinuxSyscall(
     linux_syscall_nr SyscallNumber,
@@ -300,6 +342,112 @@ local void ReleaseMemory(void* Memory, usize ReservedSize)
         ReservedSize,
         0, 0, 0, 0
     );
+}
+
+local void LinuxThreadEntry(linux_stack_header* Header)
+{
+    // TODO(vak): Investigate if `Header` might actually get overwritten because of
+    // the `red zone` used for storing register arguments.. ?
+
+    thread_id ThreadID = Header->ThreadID;
+    linux_thread* Thread = LinuxAllThreads + ThreadID;
+
+    s32 Returned = Header->Callback(Header->UserData);
+
+    __atomic_store_n(&Thread->JoinFutex, 1, __ATOMIC_SEQ_CST);
+
+    LinuxSyscall(
+        LinuxSyscallNR_Futex,
+        (usize)&Thread->JoinFutex,
+        FUTEX_WAKE,
+        S32Max,
+        0, 0, 0
+    );
+
+    // TODO(vak): Do we need a write barrier here.. ??
+
+    Thread->Returned = Returned;
+
+    ThreadExit(Returned);
+}
+
+__attribute__((naked))
+local ssize LinuxPerformClone(linux_stack_header* Header)
+{
+#if ArchitectureX64
+    __asm__ volatile (
+        "mov    %rdi,       %rsi\n"         // NOTE(vak): rsi = stack = Header
+        "mov    $0x50F00,   %rdi\n"         // NOTE(vak): rdi = flags = CLONE_VM|CLONE_FILES|CLONE_FS|CLONE_THREAD|CLONE_SYSVMEM
+        "mov    $0x38,      %rax\n"         // NOTE(vak): rax = __NR_clone = (56)
+        "syscall\n"
+        "mov    %rsp,       %rdi\n"         // NOTE(vak): Argument for LinuxThreadEntry(). Main thread will ignore this.
+        "ret\n"                             // NOTE(vak): Main thread returns as normal. Spawned thread ends up in LinuxThreadEntry().
+    );
+#else
+#   error LinuxPerformClone() is not implemented for this architecture.
+#endif
+}
+
+local thread_id SpawnThread(spawn_thread_info* Info)
+{
+    if (!Info)                                  return (false);
+    if (!Info->Callback)                        return (false);
+    if (!Info->StackBase)                       return (false);
+    if (Info->StackSize < KB(1))                return (false);
+    if (!IsAligned((usize)Info->StackBase, 64)) return (false);
+    if (!IsAligned(Info->StackSize, 64))        return (false);
+
+    if (LinuxThreadCount == LinuxMaxThreadSpawnCount)
+        return (false);
+
+    thread_id ThreadID = LinuxThreadCount;
+
+    linux_thread* Thread = LinuxAllThreads + ThreadID;
+    Thread->JoinFutex = 0;
+
+    ZeroMemory(Info->StackBase, Info->StackSize);
+
+    linux_stack_header* Header = (linux_stack_header*)
+        ((u8*)Info->StackBase + Info->StackSize - sizeof(linux_stack_header));
+
+    Header->ThreadEntryAddress  = (void*)&LinuxThreadEntry;
+    Header->Callback            = Info->Callback;
+    Header->UserData            = Info->UserData;
+    Header->ThreadID            = ThreadID;
+
+    ssize CloneResult = LinuxPerformClone(Header);
+
+    thread_id ResultID = NilThreadID;
+
+    if (CloneResult >= 0)
+    {
+        ResultID = ThreadID;
+        LinuxThreadCount++;
+    }
+
+    return (ResultID);
+}
+
+local s32 JoinThread(thread_id ThreadID)
+{
+    if (!LinuxIsValidThreadID(ThreadID))
+        return (0);
+
+    linux_thread* Thread = LinuxAllThreads + ThreadID;
+
+    LinuxSyscall(
+        LinuxSyscallNR_Futex,
+        (usize)&Thread->JoinFutex,
+        FUTEX_WAIT,
+        0, 0,
+        0, 0
+    );
+
+    s32 Returned = Thread->Returned;
+
+    ZeroType(Thread);
+
+    return (Returned);
 }
 
 local void ThreadExit(u8 ExitCode)
